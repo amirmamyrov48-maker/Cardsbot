f75beec4fa96241f4efca3dff082aa4d1dae049f
amirmamyrov48-maker <amirmamyrov48@gmail.com>
2026-06-19 14:21:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -116,3 +116,25 @@ def show_collection(message):
 if __name__ == '__main__':
     print("Бот запущен...")
     bot.infinity_polling()
+
+# --- КОСТЫЛЬ ДЛЯ БЕСПЛАТНОГО ХОСТИНГА RENDER ---
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Бот Пацанов активен и работает!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+if __name__ == '__main__':
+    keep_alive() # Запускает мини-веб-сервер в фоне
+    print("Бот запущен...")
+    bot.infinity_polling()
